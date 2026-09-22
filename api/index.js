11df082bc68c66a8451f06274f5d4790d88d0578
ZachD181 <84939280+ZachD181@users.noreply.github.com>
2026-09-22 08:07:24 -0600

    Log Resend error details

--- api/index.js
+++ api/index.js
@@ -608,7 +608,10 @@ const resetUrl =
       <p>If you did not request this, you can ignore this email.</p>
     `,
   });
-  console.log("RESEND RESULT:", resendResult);
+  console.log(
+  "RESEND ERROR:",
+  JSON.stringify(resendResult.error, null, 2)
+);
 }
 
 if (
